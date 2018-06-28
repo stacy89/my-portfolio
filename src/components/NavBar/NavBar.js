6734104aaf283d02c34691b19e5cb4b6db651603
Stacy <stacyareas@gmail.com>
2018-06-27 17:22:51 -0700

    style navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,9 +5,9 @@ import "./NavBar.css";
 const NavBar = () => (
 	<div>
 		<ul>
-			<li><Link to="/projects">Projects</Link></li>
-			<li><Link to="/photography">Photography</Link></li>
-			<li><Link to="/aboutme">About Me</Link></li>
+			<li className="navbar-link"><Link to="/projects">Projects</Link></li>
+			<li className="navbar-link"><Link to="/photography">Photography</Link></li>
+			<li className="navbar-link"><Link to="/aboutme">About Me</Link></li>
 		</ul>
 	</div>
 );
